d6fc87f9e23497ae05f4ec4703539d684458ca99
Stabilo <zteo015@e.ntu.edu.sg>
2023-02-26 16:22:34 +0800

    done with slide 3

--- mod2/test.py
+++ mod2/test.py
@@ -1,21 +1,15 @@
-ans = 0
-abs_x = 0
-neg_flag = False
-x = int(input("Enter an integer: "))
-if x < 0:
-   neg_flag = True
-   abs_x = -x
-while ans**2 < x:
-   ans = ans + 1
-if ans**2 == x:
-   print("Square root of", x, "is", ans)
-else:
-   print(x, "is not a perfect square")
-   if neg_flag:
-        while ans**2 < abs_x:
-            ans = ans + 1
-        if ans**2 == abs_x:
-            print("Square root of", x, "is", ans, "i")
-        else:  
-            # for example, if x is -5. √-5 = √5 * √-1, so answer is √5i, since √-1 = i
-            print("Square root of", x, "is", ans, "√", abs_x, "i")
+an_letters = "aefhilmnorsxAEFHILMNORSX"
+word = input("I will cheer for you! Enter a word: ")
+times = int(input("Enthusiasm level (1-10): "))
+
+i = 0
+while i < len(word):
+   char = word[i]
+   if char in an_letters:
+       print("Give me an " + char + "! " + char)
+   else:
+       print("Give me a  " + char + "! " + char)
+   i += 1
+print("What does that spell?")
+for i in range(times):
+   print(word, "!!!")
