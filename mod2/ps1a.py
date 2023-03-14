e8c0e94880d2ac1a091b2896d76419debbe783dc
Stabilo <zteo015@e.ntu.edu.sg>
2023-03-14 20:07:21 +0800

    done class exercises for lec 5

---
+++ mod2/ps1a.py
@@ -0,0 +1,11 @@
+# Assignment 1 - Part A
+# Started 14 Mar 2023 by zyteo
+# Completed x Mar 2023
+
+# Your program should ask the user to enter the following variables: 
+# 1.The starting annual salary (annual_salary) 
+annual_salary = float(input("Please enter your annual salary: "))
+# 2.The portion of salary to be saved (portion_saved) 
+portion_saved = float(input("Please enter the portion of your salary to be saved: "))
+# 3.The cost of your dream home (total_cost)
+total_cost = float(input("Please enter the cost of your dream home: "))
