7e88c43ade10a3d44595e9e203c6d67dd28a1734
Stabilo <zteo015@e.ntu.edu.sg>
2023-03-31 20:42:40 +0800

    working on lec 7 code

--- mod2/test.py
+++ mod2/test.py
@@ -1,18 +1,41 @@
-def rev_list_buggy_fix(L):
+def primes_list_buggy(n):
    """
-   input: L, a list
-   Modifies L such that its elements are in reverse order
-   returns: nothing
+   input: n an integer > 1
+   returns: list of all the primes up to and including n
    """
-   for i in range(len(L)//2):
-       j = len(L) - i - 1
-       temp = L[i]
-       L[i] = L[j]
-       L[j] = temp
-
-## list index out of range -> sub 1 to j
+   # initialize primes list
+   primes = []
+   if n >= 2:
+       primes.append(2)
+   # go through each elem of primes list
+   for i in range(2,n+1,1):
+       # go through each of 2...n
+       print("i: ", i)
+       for j in range(len(primes)):
+           print(j,primes[j])
+           # check if not divisible by elem of list
+           if i % primes[j] != 0:
+                print(primes)
+                
+                # need to adjust this line
+                primes.append(i)
+           elif i % primes[j] == 0:
         
-L = [1,2,3,4]
-# rev_list(L)
-rev_list_buggy(L)
-print(L)
+                break
+   return primes
+#
+#for a number to be prime, it is only divisible by 1 and itself
+# so n % primes[j] != 0 means that n is not divisible by any of the primes, so it is prime
+# likewise, as long as n % primes[j] == 0, n is not prime, so we can break out of the loop and move on to the next number
+
+## FIXES: --------------------------
+## division by zero -> iterate through elems not indices
+
+## forgot to return 
+## infinite loop -> append j not i
+##               -> list is getting modified as iterating over it!
+##   
+## n = 4 adds 4 -> need way to stop going once found a divisible num
+##              -> use a flag
+## --------------------------
+print(primes_list_buggy(7))
