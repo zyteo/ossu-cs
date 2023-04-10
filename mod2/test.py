b4ae02bcc4cc66a843501a3e9ece66cfba7e28f1
Stabilo <zteo015@e.ntu.edu.sg>
2023-04-10 20:56:09 +0800

    working on part 3A of assignment 2

--- mod2/test.py
+++ mod2/test.py
@@ -1,18 +1,23 @@
-import string
-def get_available_letters(letters_guessed):
+def match_with_gaps(my_word, other_word):
     """
-    letters_guessed: list (of letters), which letters have been guessed so far
-    returns: string (of letters), comprised of letters that represents which letters have not
-      yet been guessed.
+    my_word: string with _ characters, current guess of secret word
+    other_word: string, regular English word
+    returns: boolean, True if all the actual letters of my_word match the
+        corresponding letters of other_word, or the letter is the special symbol
+        _ , and my_word and other_word are of the same length;
+        False otherwise:
     """
-    # first get all lowercase letters of the alphabet with string package
-    # then replace the letters in letters_guessed with ""
-    remaining_letters = string.ascii_lowercase
-    for alphabet in letters_guessed:
-        remaining_letters = remaining_letters.replace(alphabet, "")
-    return remaining_letters
+    # first strip my_word of spaces
+    # then compare length of my_word and other_word, if not same, return false
+    # compare each letter of my_word and other_word, if not same, return false
+    my_word = my_word.replace(" ", "")
+    print(my_word)
+    if len(my_word) != len(other_word):
+        print("lengths not same", len(my_word), len(other_word))
+        return False
+    else:
+        for i in range(len(my_word)):
+            print(my_word[i], other_word[i])
 
 
-secret_word = "apple"
-letters_guessed = ["e", "i", "k", "p", "r", "s"]
-print(get_available_letters(letters_guessed))
+match_with_gaps("t__t", "tact")
