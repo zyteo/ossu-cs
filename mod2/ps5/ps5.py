3eb6460d431bf223a6db7624e189785c86f55945
Stabilo <zteo015@e.ntu.edu.sg>
2023-05-10 20:45:11 +0800

    modified phrasetrigger function as it was not accounting for punctuation

--- mod2/ps5/ps5.py
+++ mod2/ps5/ps5.py
@@ -15,13 +15,14 @@ from datetime import datetime
 import pytz
 
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
-#======================
+# ======================
 # Code for retrieving and parsing
 # Google and Yahoo News feeds
 # Do not change this code
-#======================
+# ======================
+
 
 def process(url):
     """
@@ -41,8 +42,8 @@ def process(url):
         try:
             pubdate = datetime.strptime(pubdate, "%a, %d %b %Y %H:%M:%S %Z")
             pubdate.replace(tzinfo=pytz.timezone("GMT"))
-          #  pubdate = pubdate.astimezone(pytz.timezone('EST'))
-          #  pubdate.replace(tzinfo=None)
+        #  pubdate = pubdate.astimezone(pytz.timezone('EST'))
+        #  pubdate.replace(tzinfo=None)
         except ValueError:
             pubdate = datetime.strptime(pubdate, "%a, %d %b %Y %H:%M:%S %z")
 
@@ -50,11 +51,13 @@ def process(url):
         ret.append(newsStory)
     return ret
 
-#======================
+
+# ======================
 # Data structure design
-#======================
+# ======================
 
 # Problem 1
+
 
 # TODO: NewsStory
 # ●globally unique identifier (GUID) - a string
@@ -74,20 +77,27 @@ class NewsStory(object):
         self.description = description
         self.link = link
         self.pubdate = pubdate
+
     def get_guid(self):
         return self.guid
+
     def get_title(self):
         return self.title
+
     def get_description(self):
         return self.description
+
     def get_link(self):
         return self.link
+
     def get_pubdate(self):
         return self.pubdate
 
-#======================
+
+# ======================
 # Triggers
-#======================
+# ======================
+
 
 class Trigger(object):
     def evaluate(self, story):
@@ -98,22 +108,25 @@ class Trigger(object):
         # DO NOT CHANGE THIS!
         raise NotImplementedError
 
+
 # PHRASE TRIGGERS
+
 
 # Problem 2
 # TODO: PhraseTrigger
-# Implement a phrase trigger abstract class, PhraseTrigger . 
+# Implement a phrase trigger abstract class, PhraseTrigger .
 # It should take in a string phraseas an argument to the class's constructor. This trigger should not be case-sensitive (it should treat "Intel" and "intel" as being equal).
-# PhraseTrigger should be a subclass of Trigger . It has one new method,is_phrase_in , which takes in one string argument text. 
-# It returns True if the whole phrasephrase is present in text, 
+# PhraseTrigger should be a subclass of Trigger . It has one new method,is_phrase_in , which takes in one string argument text.
+# It returns True if the whole phrasephrase is present in text,
 # False otherwise, as described in the above examples.
-#  Thismethod should not be case-sensitive. 
+#  Thismethod should not be case-sensitive.
 # Implement this method. Because this is an abstract class, we will not be directly instantiating any PhraseTriggers .
-# PhraseTrigger should inherit its evaluate method from Trigger . 
+# PhraseTrigger should inherit its evaluate method from Trigger .
 # We do this because nowwe can create subclasses of PhraseTrigger that use its is_phrase_in function.✦✦✦✦You are now ready to implement PhraseTrigger 's two subclasses: TitleTrigger andDescriptionTrigger .
 class PhraseTrigger(Trigger):
     def __init__(self, phrase):
         self.phrase = phrase
+
     def is_phrase_in(self, text):
         # need to compare phrase to text
         # first convert both to lowercase
@@ -126,19 +139,33 @@ class PhraseTrigger(Trigger):
         phrase = self.phrase.lower()
         text = text.lower()
         text = text.split()
+        # create a new list to store words
+        new_text = []
+        # get list of punctuation
+        punctuation = string.punctuation
         for word in text:
-            word = word.replace(string.punctuation, "")
-        text = " ".join(text)
+            # remove punctuation
+            for i in punctuation:
+                if i in word:
+                    word = word.replace(i, "")
+            new_text.append(word)
+        text = " ".join(new_text)
         if phrase in text:
             return True
         else:
             return False
+
 
 # test phrase trigger
 phrase = "Intel"
 text = "Intel is a company"
 phrase_trigger = PhraseTrigger(phrase)
 print(phrase_trigger.is_phrase_in(text))
+
+phrase2 = "purple cow"
+text2 = "Purple!!! Cow!!!"
+phrase_trigger2 = PhraseTrigger(phrase2)
+print(phrase_trigger2.is_phrase_in(text2))
 
 # Problem 3
 # TODO: TitleTrigger
@@ -170,9 +197,10 @@ print(phrase_trigger.is_phrase_in(text))
 # TODO: OrTrigger
 
 
-#======================
+# ======================
 # Filtering
-#======================
+# ======================
+
 
 # Problem 10
 def filter_stories(stories, triggerlist):
@@ -187,10 +215,9 @@ def filter_stories(stories, triggerlist):
     return stories
 
 
-
-#======================
+# ======================
 # User-Specified Triggers
-#======================
+# ======================
 # Problem 11
 def read_trigger_config(filename):
     """
@@ -201,22 +228,22 @@ def read_trigger_config(filename):
     """
     # We give you the code to read in the file and eliminate blank lines and
     # comments. You don't need to know how it works for now!
-    trigger_file = open(filename, 'r')
+    trigger_file = open(filename, "r")
     lines = []
     for line in trigger_file:
         line = line.rstrip()
-        if not (len(line) == 0 or line.startswith('//')):
+        if not (len(line) == 0 or line.startswith("//")):
             lines.append(line)
 
     # TODO: Problem 11
     # line is the list of lines that you need to parse and for which you need
     # to build triggers
 
-    print(lines) # for now, print it so you see what it contains!
+    print(lines)  # for now, print it so you see what it contains!
 
 
+SLEEPTIME = 120  # seconds -- how often we poll
 
-SLEEPTIME = 120 #seconds -- how often we poll
 
 def main_thread(master):
     # A sample trigger list - you might need to change the phrases to correspond
@@ -229,39 +256,47 @@ def main_thread(master):
         triggerlist = [t1, t4]
 
         # Problem 11
-        # TODO: After implementing read_trigger_config, uncomment this line 
+        # TODO: After implementing read_trigger_config, uncomment this line
         # triggerlist = read_trigger_config('triggers.txt')
-        
+
         # HELPER CODE - you don't need to understand this!
         # Draws the popup window that displays the filtered stories
         # Retrieves and filters the stories from the RSS feeds
         frame = Frame(master)
         frame.pack(side=BOTTOM)
         scrollbar = Scrollbar(master)
-        scrollbar.pack(side=RIGHT,fill=Y)
+        scrollbar.pack(side=RIGHT, fill=Y)
 
         t = "Google & Yahoo Top News"
         title = StringVar()
         title.set(t)
         ttl = Label(master, textvariable=title, font=("Helvetica", 18))
         ttl.pack(side=TOP)
-        cont = Text(master, font=("Helvetica",14), yscrollcommand=scrollbar.set)
+        cont = Text(master, font=("Helvetica", 14), yscrollcommand=scrollbar.set)
         cont.pack(side=BOTTOM)
-        cont.tag_config("title", justify='center')
+        cont.tag_config("title", justify="center")
         button = Button(frame, text="Exit", command=root.destroy)
         button.pack(side=BOTTOM)
         guidShown = []
+
         def get_cont(newstory):
             if newstory.get_guid() not in guidShown:
-                cont.insert(END, newstory.get_title()+"\n", "title")
-                cont.insert(END, "\n---------------------------------------------------------------\n", "title")
+                cont.insert(END, newstory.get_title() + "\n", "title")
+                cont.insert(
+                    END,
+                    "\n---------------------------------------------------------------\n",
+                    "title",
+                )
                 cont.insert(END, newstory.get_description())
-                cont.insert(END, "\n*********************************************************************\n", "title")
+                cont.insert(
+                    END,
+                    "\n*********************************************************************\n",
+                    "title",
+                )
                 guidShown.append(newstory.get_guid())
 
         while True:
-
-            print("Polling . . .", end=' ')
+            print("Polling . . .", end=" ")
             # Get stories from Google's Top Stories RSS news feed
             stories = process("http://news.google.com/news?output=rss")
 
@@ -273,7 +308,6 @@ def main_thread(master):
             list(map(get_cont, stories))
             scrollbar.config(command=cont.yview)
 
-
             print("Sleeping...")
             time.sleep(SLEEPTIME)
 
@@ -281,10 +315,9 @@ def main_thread(master):
         print(e)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     root = Tk()
     root.title("Some RSS parser")
     t = threading.Thread(target=main_thread, args=(root,))
     t.start()
     root.mainloop()
-
